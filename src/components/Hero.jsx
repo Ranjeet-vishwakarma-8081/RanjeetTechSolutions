6c1f79701ac4eb91ffe487c9dbc2f22943ff628f
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-04-18 10:17:53 +0530

    Added the Spotlight component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,36 @@
+import { cn } from "../lib/utils";
+import { Spotlight } from "./ui/Spotlight";
+
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div></div>
+      <div>
+        {/* First spotlight is original */}
+        {/* <Spotlight
+          className="left-0 h-screen -top-40 md:left-60 md:-top-20"
+          fill="white"
+        /> */}
+        <Spotlight
+          className="h-screen -top-40 -left-10 md:-top-20 md:-left-32"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className="relative flex items-center justify-center w-full h-screen bg-white dark:bg-black-100">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "[background-size:40px_40px]",
+            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+          )}
+        />
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+      </div>
     </div>
   );
 };
