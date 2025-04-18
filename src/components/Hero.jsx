f6747043ab76b5ba64d6a7ca6e0bf1a00b14a1e0
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-04-18 17:00:56 +0530

    Adjusted the background box's size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,23 +27,23 @@ const Hero = () => {
         <div
           className={cn(
             "absolute inset-0",
-            "[background-size:40px_40px]",
+            "[background-size:100px_100px]",
             "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+            "dark:[background-image:linear-gradient(to_right,#262626_0px,transparent_1px),linear-gradient(to_bottom,#262626_0px,transparent_1px)]"
           )}
         />
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
       </div>
 
       <div className="relative z-10 flex justify-center my-20">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex justify-center items-center flex-col">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[50vw] flex justify-center items-center flex-col">
           <h2 className="text-xs tracking-widest text-center uppercase dark:text-blue-100 max-w-80">
             Dynamic Web Magic with React.js
           </h2>
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="We develop website that ignite your Businesses"
+            words="We develop websites that ignite your Business"
           />
 
           <p className="mb-4 text-sm text-center dark:text-white md:tracking-wider md:text-lg lg:text-2xl">
