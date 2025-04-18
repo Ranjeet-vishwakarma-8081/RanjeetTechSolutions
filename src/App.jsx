bb0b8090b3cccbca97f3a16f8444c2b8390a8156
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-04-18 22:51:14 +0530

    Added the Floating Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { FaHome } from "react-icons/fa";
 import Hero from "./components/Hero";
+import { FloatingNav } from "./components/ui/FloatingNav";
 
 const App = () => {
   return (
     <main className="relative flex flex-col items-center justify-center px-5 mx-auto overflow-hidden bg-black-100 sm:px-10">
       <div className="w-full max-w-7xl">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
       </div>
     </main>
