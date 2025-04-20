a59fbc095a981f4addf858294cc3515b78cc940d
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-04-20 21:27:11 +0530

    Modified the navigation compoent's props and added the RecentProjects component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { FaHome } from "react-icons/fa";
 import Hero from "./components/Hero";
 import { FloatingNav } from "./components/ui/FloatingNav";
 import { BentoGridDemo } from "./components/BentoGridDemo";
+import RecentProjects from "./components/RecentProjects";
+import { navItems } from "./data";
 
 const App = () => {
   return (
     <main className="relative flex flex-col items-center justify-center px-5 mx-auto overflow-hidden bg-black-100 sm:px-10 ">
       <div className="w-full max-w-7xl">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <BentoGridDemo />
+        <RecentProjects />
       </div>
     </main>
   );
