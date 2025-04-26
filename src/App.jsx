3447d851395c1853b7d79fcda658faf9f1ad23ce
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-04-26 12:46:17 +0530

    Added the react-router to get the URL based scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { FaHome } from "react-icons/fa";
-import Hero from "./components/Hero";
+import { Routes, Route } from "react-router";
 import { FloatingNav } from "./components/ui/FloatingNav";
-import { Services } from "./components/Services";
-import RecentProjects from "./components/RecentProjects";
 import { navItems } from "./data";
-import Testimonials from "./components/Testimonials";
-import Experience from "./components/Experience";
-import Approach from "./components/Approach";
-import Contact from "./components/Contact";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <main className="relative flex flex-col items-center justify-center px-5 mx-auto font-sans overflow-clip bg-black-100 sm:px-10">
       <div className="w-full max-w-7xl">
         <FloatingNav navItems={navItems} />
-        <Hero />
-        <Services />
-        <RecentProjects />
-        <Testimonials />
-        <Experience />
-        <Approach />
-        <Contact />
+        <Routes>
+          <Route path="/" element={<Home scrollTo="home" />} />
+          <Route path="/services" element={<Home scrollTo="services" />} />
+          <Route path="/projects" element={<Home scrollTo="projects" />} />
+          <Route
+            path="/testimonials"
+            element={<Home scrollTo="testimonials" />}
+          />
+          <Route path="/contact" element={<Home scrollTo="contact" />} />
+        </Routes>
       </div>
     </main>
   );
--- src/components/ui/FloatingNav.jsx
+++ src/components/ui/FloatingNav.jsx
@@ -6,6 +6,7 @@ import {
   useMotionValueEvent,
 } from "motion/react";
 import { cn } from "../../lib/utils";
+import { Link } from "react-router";
 
 export const FloatingNav = ({ navItems, className }) => {
   const { scrollYProgress } = useScroll();
@@ -49,9 +50,9 @@ export const FloatingNav = ({ navItems, className }) => {
         )}
       >
         {navItems.map((navItem, idx) => (
-          <a
+          <Link
             key={`link=${idx}`}
-            href={navItem.link}
+            to={navItem.link}
             className={cn(
               "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
@@ -59,7 +60,7 @@ export const FloatingNav = ({ navItems, className }) => {
             <span className="text-xs sm:text-sm !cursor-pointer">
               {navItem.name}
             </span>
-          </a>
+          </Link>
         ))}
       </motion.div>
     </AnimatePresence>
