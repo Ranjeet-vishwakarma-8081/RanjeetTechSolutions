ff4f711d931c00468fb1ac8b60b3360b78a5a44c
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-04-27 16:45:03 +0530

    Added BackgroundBeanmWithCollision component for whatsApp Widget chat

--- src/components/WhatsAppWidget.jsx
+++ src/components/WhatsAppWidget.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
+import { BackgroundBeamsWithCollision } from "./ui/BackgroundBeamsWithCollision";
 
 const WhatsAppWidget = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -59,7 +60,7 @@ const WhatsAppWidget = () => {
         className={`fixed bottom-5 right-5 bg-green-500 p-4 rounded-full shadow-lg hover:bg-green-600 transition-all duration-300 z-50 ${
           shouldBounce && !isOpen ? "animate-bounce" : ""
         }
-           ${isVisible ? "opacity-100 scale-100" : "opacity-0 scale-90"}`}
+        ${isVisible ? "opacity-100 scale-100" : "opacity-0 scale-90"}`}
       >
         {isOpen ? (
           <IoClose className="text-white size-6" />
@@ -78,20 +79,27 @@ const WhatsAppWidget = () => {
 
       {/* Chat Popup */}
       {isOpen && (
-        <div className="fixed z-50 p-4 transition-all duration-300 bg-white shadow-xl w-72 bottom-20 right-5 rounded-xl animate-fade-in">
-          <h3 className="mb-2 text-lg font-semibold">Chat with Ranjeet 💬</h3>
-          <p className="mb-4 text-sm text-gray-600">
-            Hello! 👋 <br />
-            Have questions or collaboration ideas? Let’s connect on WhatsApp.
-          </p>
-          <a
-            href={`https://wa.me/${phoneNumber}?text=${encodedMessage}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block py-2 font-semibold text-center text-white transition-colors duration-300 bg-green-500 rounded-md hover:bg-green-600"
-          >
-            Start Chat
-          </a>
+        <div className="fixed z-50 transition-all duration-300 shadow-xl w-72 bottom-20 right-5 animate-fade-in">
+          <BackgroundBeamsWithCollision>
+            <div className="p-4 bg-white rounded-xl ">
+              <h3 className="mb-2 text-lg font-semibold">
+                Chat with Ranjeet 💬
+              </h3>
+              <p className="mb-4 text-sm text-gray-600">
+                Hello! 👋 <br />
+                Have questions or collaboration ideas? Let’s connect on
+                WhatsApp.
+              </p>
+              <a
+                href={`https://wa.me/${phoneNumber}?text=${encodedMessage}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block py-2 font-semibold text-center text-white transition-colors duration-300 bg-green-500 rounded-md hover:bg-green-600"
+              >
+                Start Chat
+              </a>
+            </div>
+          </BackgroundBeamsWithCollision>
         </div>
       )}
     </>
