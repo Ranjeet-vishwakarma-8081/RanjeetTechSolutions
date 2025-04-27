13819fb062a617eb8768b697bd4ae924d51f160e
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-04-27 14:02:48 +0530

    Added the whatsapp widget for seamless connectivity

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router";
 import { FloatingNav } from "./components/ui/FloatingNav";
 import { navItems } from "./data";
 import Home from "./components/Home";
+import WhatsAppWidget from "./components/WhatsAppWidget";
 
 const App = () => {
   return (
     <main className="relative flex flex-col items-center justify-center px-5 mx-auto font-sans overflow-clip bg-black-100 sm:px-10">
       <div className="w-full max-w-7xl">
         <FloatingNav navItems={navItems} />
+        <WhatsAppWidget />
         <Routes>
           <Route path="/" element={<Home scrollTo="home" />} />
           <Route path="/services" element={<Home scrollTo="services" />} />
